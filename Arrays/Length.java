419e69a321d4a80989537cb17ece5539ed8877a9
Tejas Sandeep Khurd <officialtejaspro@gmail.com>
2025-07-25 09:23:50 +0530

    Array
    Basics of Array

---
+++ Arrays/Length.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Length {
+    int[] arr = {1, 2, 3};
+    int len = arr.length;
+
+    public static void main(String[] args) {
+        Length obj = new Length();
+        System.out.println(obj.len);
+    }
+}
